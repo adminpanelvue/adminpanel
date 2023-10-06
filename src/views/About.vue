b2b96b845cbc6aaf92eb4fc014e23999132bba41
furkan-aksakal <85026220+furkan-aksakal@users.noreply.github.com>
2023-10-06 14:03:53 +0300

    color changes at about

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,9 +1,9 @@
 <template>
     <div>
-        <h1>
+        <h1 style="color: rgb(33, 33, 145);">
             Welcome!
         </h1>
-        <h2>
+        <h2 style="color: rgb(14, 14, 72);">
             Ajet Admin Panel
         </h2>
     </div>
