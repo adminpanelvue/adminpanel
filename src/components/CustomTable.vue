b726f182208aedbf6083cd7926a4d03925a40985
furkan-aksakal <85026220+furkan-aksakal@users.noreply.github.com>
2023-10-08 02:34:29 +0300

    add butonu için farklı bir dialog implement edildi

--- src/components/CustomTable.vue
+++ src/components/CustomTable.vue
@@ -3,8 +3,58 @@
 
         <v-row class="pa-4 align-center">
             <div :class="['text-h4', 'pa-2']">i18n</div>
-            <v-spacer></v-spacer>
-            <v-btn class="bg-white" @click="dialog = true">Add</v-btn>
+            <v-dialog v-model="dialogAdd" persistent width="1024">
+                <template v-slot:activator="{ props }">
+                    <v-col class="text-right">
+                        <v-btn v-bind="props" class="bg-white" @click="dialogAdd = true">Add</v-btn>
+                    </v-col>
+
+                </template>
+
+                <v-card>
+                    <v-card-title>
+                        <span class="text-h5">Add i18n</span>
+                    </v-card-title>
+                    <v-card-text>
+                        <v-container>
+                            <v-row>
+                                <v-col cols="12" sm="6" md="4">
+                                    <v-text-field v-model="user.firstName" label="ID*" required value></v-text-field>
+                                </v-col>
+                                <v-col cols="12" sm="6" md="4">
+                                    <v-select :items="['tr', 'eng']" label="Lang*" required></v-select>
+                                </v-col>
+                                <v-col cols="12" sm="6" md="4">
+                                    <v-text-field v-model="user.username" label="Key*"
+                                        hint="example of persistent helper text" persistent-hint required></v-text-field>
+                                </v-col>
+                                <v-col cols="12" sm="6" md="4">
+                                    <v-text-field v-model="user.username" label="Value*"
+                                        hint="example of persistent helper text" persistent-hint required></v-text-field>
+                                </v-col>
+
+                            </v-row>
+                        </v-container>
+                        <small>*indicates required field</small>
+                    </v-card-text>
+
+                    <v-card-actions>
+                        <v-spacer></v-spacer>
+
+                        <v-btn text="Close" @click="dialogAdd = false"></v-btn>
+
+                        <v-btn :loading="btnLoading" color="blue-darken-1" variant="text" @click="addUser">
+                            Save
+                        </v-btn>
+                    </v-card-actions>
+
+
+                </v-card>
+
+
+            </v-dialog>
+
+
         </v-row>
 
 
@@ -68,7 +118,7 @@
                                     <v-text-field v-model="user.username" label="Value*"
                                         hint="example of persistent helper text" persistent-hint required></v-text-field>
                                 </v-col>
-                                
+
                             </v-row>
                         </v-container>
                         <small>*indicates required field</small>
@@ -94,6 +144,7 @@ export default {
     data: () => ({
         itemsPerPage: 10,
         dialog: false,
+        dialogAdd: false,
         user: {
             firstName: "",
             lastName: "",
@@ -157,6 +208,9 @@ export default {
         async editUser(item) {
             this.user = item;
             this.dialog = true;
+        },
+        async addUser() {
+            this.dialogAdd = false;
         }
     },
 }
