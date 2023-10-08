1d6ed791df41fc2c464b347dd55072be6cddbe70
furkan-aksakal <85026220+furkan-aksakal@users.noreply.github.com>
2023-10-08 19:45:38 +0300

    aboutta ufak tefek şeyler yapıldı

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,4 +1,16 @@
 <template>
+    <v-app-bar>
+        <v-container class="bg-blue">
+
+            <v-row class="pa-4 align-center">
+                <div :class="['text-h4', 'pa-2']">About</div>
+                <v-spacer></v-spacer>
+
+            </v-row>
+        
+        </v-container>
+    </v-app-bar>
+
     <div>
         <h1 style="color: rgb(33, 33, 145); text-align: center;">
             Welcome!
@@ -10,6 +22,6 @@
             <p style="text-align:center">@anadolujet</p>
         </a>
         <v-banner></v-banner>
-        
+
     </div>
 </template>
