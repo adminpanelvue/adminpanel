7946ecc1c471ec60ef37d2e1f0bc7166149cd77f
furkan-aksakal <85026220+furkan-aksakal@users.noreply.github.com>
2023-10-09 10:15:11 +0300

    AppConfig list, labels ve i18n ile senkronize edildi

--- src/components/CustomAppConfigListTable.vue
+++ src/components/CustomAppConfigListTable.vue
@@ -1,24 +1,69 @@
 <template>
+    <v-app-bar>
+        <v-container class="bg-blue">
 
-    <v-container  class="bg-blue">
-        
-        <v-row class="pa-4 align-center" >
-            <div :class="['text-h4', 'pa-2']">AppConfig List</div>
-            <v-spacer></v-spacer>
-            <v-btn class="bg-white" @click="dialog = true">Add</v-btn>
-        </v-row>
-            
-        
-    </v-container>
-    <v-snackbar
-      v-model="snackbar"
-      location="center"
-    >
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi, ratione debitis quis est labore voluptatibus! Eaque cupiditate minima, at placeat totam, magni doloremque veniam neque porro libero rerum unde voluptatem!
+            <v-row class="pa-4 align-center">
+                <div :class="['text-h4', 'pa-2']">AppConfig List</div>
+                <v-dialog v-model="dialogAdd" persistent width="1024">
+                    <template v-slot:activator="{ props }">
+                        <v-col class="text-right">
+                            <v-btn v-bind="props" class="bg-white" @click="dialogAdd = true">Add</v-btn>
+                        </v-col>
 
-      <template v-slot:actions>
-        <v-btn @click = "snackbar = false">Close</v-btn>
-      </template>
+                    </template>
+
+                    <v-card>
+                        <v-card-title>
+                            <span class="text-h5">Add Labels</span>
+                        </v-card-title>
+                        <v-card-text>
+                            <v-container>
+                                <v-row>
+                                    <v-col cols="12">
+                                        <v-text-field label="Name*" hint="example of persistent helper text" persistent-hint
+                                            required></v-text-field>
+                                    </v-col>
+                                    <v-col cols="12">
+                                        <v-text-field label="Value*" required></v-text-field>
+                                    </v-col>
+
+                                </v-row>
+                            </v-container>
+                            <small>*indicates required field</small>
+                        </v-card-text>
+
+                        <v-card-actions>
+                            <v-spacer></v-spacer>
+
+                            <v-btn text="Close" @click="dialogAdd = false"></v-btn>
+
+                            <v-btn :loading="btnLoading" color="blue-darken-1" variant="text" @click="save">
+                                Save
+                            </v-btn>
+                        </v-card-actions>
+
+
+                    </v-card>
+
+
+                </v-dialog>
+
+
+            </v-row>
+
+
+        </v-container>
+    </v-app-bar>
+
+    <v-snackbar v-model="snackbar" location="center">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi, ratione debitis quis est labore
+        voluptatibus!
+        Eaque cupiditate minima, at placeat totam, magni doloremque veniam neque porro libero rerum unde
+        voluptatem!
+
+        <template v-slot:actions>
+            <v-btn @click="snackbar = false">Close</v-btn>
+        </template>
     </v-snackbar>
 
     <v-data-table-server v-model:items-per-page="itemsPerPage" :headers="headers" :items-length="length" :items="items"
@@ -36,7 +81,7 @@
             <v-icon size="small" class="me-2" @click="dialog = true">
                 mdi-pencil
             </v-icon>
-            <v-icon size="small">
+            <v-icon size="small" class="me-2" @click="deleteUser(item)">
                 mdi-delete
             </v-icon>
         </template>
@@ -57,14 +102,14 @@
                         <v-container>
                             <v-row>
                                 <v-col cols="12">
-                                    <v-text-field label="Name*" hint="example of persistent helper text"
-                                        persistent-hint required></v-text-field>
+                                    <v-text-field label="Name*" hint="example of persistent helper text" persistent-hint
+                                        required></v-text-field>
                                 </v-col>
                                 <v-col cols="12">
                                     <v-text-field label="Value*" required></v-text-field>
                                 </v-col>
-                                
-                                
+
+
                             </v-row>
                         </v-container>
                         <small>*indicates required field</small>
@@ -74,9 +119,20 @@
                         <v-btn color="blue-darken-1" variant="text" @click="dialog = false">
                             Close
                         </v-btn>
-                        <v-btn :loading= "btnLoading" color="blue-darken-1" variant="text" @click="save">
+                        <v-btn :loading="btnLoading" color="blue-darken-1" variant="text" @click="save">
                             Save
                         </v-btn>
+                    </v-card-actions>
+                </v-card>
+            </v-dialog>
+            <v-dialog v-model="dialogDelete" max-width="500px">
+                <v-card>
+                    <v-card-title class="text-h5">Are you sure you want to delete this item?</v-card-title>
+                    <v-card-actions>
+                        <v-spacer></v-spacer>
+                        <v-btn color="blue-darken-1" variant="text" @click="dialogDelete = false">Cancel</v-btn>
+                        <v-btn color="blue-darken-1" variant="text" @click="dialogDelete = false">OK</v-btn>
+                        <v-spacer></v-spacer>
                     </v-card-actions>
                 </v-card>
             </v-dialog>
@@ -90,6 +146,8 @@ export default {
     data: () => ({
         itemsPerPage: 10,
         dialog: false,
+        dialogAdd: false,
+        dialogDelete: false,
         headers: [
             { title: 'ID', key: 'id' },
             {
@@ -112,7 +170,7 @@ export default {
         loading: true,
         length: 0,
         snackbar: false,
-        btnLoading:false,
+        btnLoading: false,
     }),
     methods: {
         async loadItems() {
@@ -129,7 +187,7 @@ export default {
                 this.snackbar = true;
             }
         },
-        async save(){
+        async save() {
             try {
                 this.loading = true;
                 this.btnLoading = true;
@@ -141,13 +199,16 @@ export default {
             } catch (error) {
                 this.snackbar = true;
             }
-            
+
 
         },
-        async editUser(item){
+        async editUser(item) {
             this.user = item;
             this.dialog = true;
-        }
+        },
+        async deleteUser() {
+            this.dialogDelete = true;
+        },
     },
 }
 </script>
