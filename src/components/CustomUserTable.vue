ace7929be4b4559a9e73fe1d6bcc5e2e5c8bfd86
furkan-aksakal <85026220+furkan-aksakal@users.noreply.github.com>
2023-10-09 10:41:46 +0300

    User table diğer tablelarla senkronize edildi

--- src/components/CustomUserTable.vue
+++ src/components/CustomUserTable.vue
@@ -1,15 +1,75 @@
 <template>
-    <v-container class="bg-blue">
+    <v-app-bar>
+        <v-container class="bg-blue">
 
-        <v-row class="pa-4 align-center">
-            <div :class="['text-h4', 'pa-2']">Users</div>
-            <v-spacer></v-spacer>
-            <v-btn class="bg-white" @click="dialog = true">Add</v-btn>
-        </v-row>
+            <v-row class="pa-4 align-center">
+                <div :class="['text-h4', 'pa-2']">Users</div>
+                <v-dialog v-model="dialogAdd" persistent width="1024">
+                    <template v-slot:activator="{ props }">
+                        <v-col class="text-right">
+                            <v-btn v-bind="props" class="bg-white" @click="dialogAdd = true">Add</v-btn>
+                        </v-col>
+
+                    </template>
+
+                    <v-card>
+                        <v-card-title>
+                            <span class="text-h5">Add i18n</span>
+                        </v-card-title>
+                        <v-card-text>
+                            <v-container>
+                                <v-row>
+                                    <v-col cols="12" sm="6" md="4">
+                                        <v-text-field v-model="user.firstName" label="First Name*" required
+                                            value></v-text-field>
+                                    </v-col>
+                                    <v-col cols="12" sm="6" md="4">
+                                        <v-text-field v-model="user.lastName" label="Last Name*"
+                                            hint="example of persistent helper text" persistent-hint
+                                            required></v-text-field>
+                                    </v-col>
+                                    <v-col cols="12" sm="6" md="4">
+                                        <v-text-field v-model="user.username" label="Username*"
+                                            hint="example of persistent helper text" persistent-hint
+                                            required></v-text-field>
+                                    </v-col>
+                                    <v-col cols="12">
+                                        <v-text-field label="Email*" required></v-text-field>
+                                    </v-col>
+                                    <v-col cols="12" sm="6" md="6">
+                                        <v-text-field label="Role*" required></v-text-field>
+                                    </v-col>
+                                    <v-col cols="12">
+                                        <v-text-field label="Password*" type="password" required></v-text-field>
+                                    </v-col>
+
+                                </v-row>
+                            </v-container>
+                            <small>*indicates required field</small>
+                        </v-card-text>
+
+                        <v-card-actions>
+                            <v-spacer></v-spacer>
+
+                            <v-btn text="Close" @click="dialogAdd = false"></v-btn>
+
+                            <v-btn :loading="btnLoading" color="blue-darken-1" variant="text" @click="save">
+                                Save
+                            </v-btn>
+                        </v-card-actions>
 
 
-    </v-container>
+                    </v-card>
 
+
+                </v-dialog>
+
+
+            </v-row>
+
+
+        </v-container>
+    </v-app-bar>
 
 
     <v-snackbar v-model="snackbar" location="center">
@@ -35,7 +95,7 @@
             <v-icon size="small" class="me-2" @click="editUser(item)">
                 mdi-pencil
             </v-icon>
-            <v-icon size="small">
+            <v-icon size="small" class="me-2" @click="deleteUser(item)">
                 mdi-delete
             </v-icon>
         </template>
@@ -106,6 +166,17 @@
                     </v-card-actions>
                 </v-card>
             </v-dialog>
+            <v-dialog v-model="dialogDelete" max-width="500px">
+                <v-card>
+                    <v-card-title class="text-h5">Are you sure you want to delete this item?</v-card-title>
+                    <v-card-actions>
+                        <v-spacer></v-spacer>
+                        <v-btn color="blue-darken-1" variant="text" @click="dialogDelete = false">Cancel</v-btn>
+                        <v-btn color="blue-darken-1" variant="text" @click="dialogDelete = false">OK</v-btn>
+                        <v-spacer></v-spacer>
+                    </v-card-actions>
+                </v-card>
+            </v-dialog>
         </v-row>
     </template>
 </template>
@@ -116,6 +187,8 @@ export default {
     data: () => ({
         itemsPerPage: 10,
         dialog: false,
+        dialogAdd: false,
+        dialogDelete: false,
         user: {
             firstName: "",
             lastName: "",
@@ -181,7 +254,10 @@ export default {
         async editUser(item) {
             this.user = item;
             this.dialog = true;
-        }
+        },
+        async deleteUser() {
+            this.dialogDelete = true;
+        },
     },
 }
 </script>
